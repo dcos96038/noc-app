28d2c6e3a47c9149b233f1be193c41e32f2fbb9c
Diego Coscolla <diegocoscolla@gmail.com>
2023-11-08 16:51:55 -0300

    Add MongoDB integration and log datasource

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { envs } from './config/plugins/envs.plugin';
+import { MongoDatabase } from './data/mongo/init';
 import { Server } from './presentation/server';
 
 
@@ -5,6 +7,12 @@ import { Server } from './presentation/server';
   main();
 })();
 
-function main()  {
+async function main()  {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
+
+
   Server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,13 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendLogsEmail } from "../domain/use-cases/email/send-logs-email";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron";
 import { EmailService } from "./email/email.service";
 
-const logRepository = new LogRepositoryImpl(new FileSystemDatasource())
+const logRepository = new LogRepositoryImpl(new MongoLogDatasource())
+// const logRepository = new LogRepositoryImpl(new FileSystemDatasource())
 const emailService = new EmailService();
 
 export class Server {
@@ -13,15 +15,6 @@ export class Server {
     console.log('Server started');
 
     new SendLogsEmail(logRepository, emailService).execute('diegocoscolla@gmail.com')
-
-    // emailService.sendEmail({
-    //   to: 'diegocoscolla@gmail.com',
-    //   subject: 'Test',
-    //   htmlBody: '<h1>Test</h1>'
-    // })
-
-    // emailService.sendEmailWithFileSystemLogs('diegocoscolla@gmail.com')
-
 
     CronService.createJob('*/5 * * * * *', () => {
       const checkService = new CheckService(logRepository, undefined, undefined);
